8913b92e2effb31fc1f3639bf76189bcf441f1db
Eric Pinet <pineri01@gmail.com>
2015-10-10 13:22:18 -0400

    Adding the simulator in the same repository.

---
+++ simulator/src/main/java/com/connectlife/simulator/MainWindow.java
@@ -0,0 +1,289 @@
+/**
+ *  MainWindow.java
+ *  simulator
+ *
+ *  Created by ericpinet on 2015-09-13.
+ *  Copyright (c) 2015 ConnectLife (Eric Pinet). All rights reserved.
+ *
+ */
+package com.connectlife.simulator;
+
+// external
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Label;
+import java.util.Iterator;
+import org.apache.thrift.TException;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import com.google.gson.Gson;
+
+// internal
+import com.connectlife.coreserver.modules.environment.data.*;
+import com.connectlife.clapi.*;
+import com.connectlife.clapi.CLApiPush.Processor;
+import com.connectlife.clapi.client.Client;
+import com.connectlife.clapi.client.NotificationListener;
+
+/**
+ * 
+ * 
+ * @author ericpinet
+ * <br> 2015-09-13
+ */
+public class MainWindow implements NotificationListener {
+
+	protected Shell shell;
+	private Text textHost;
+	private Text textPort;
+	private Label lblStatus;
+	private Client client;
+	
+	private static final String HOST = "127.0.0.1";
+	
+	private static final String PORT = "9006";
+	
+	/**
+	 * Start Person window.
+	 * 
+	 * @author ericpinet
+	 * <br> 2015-09-13
+	 */
+	static class startPerson implements Runnable{
+		public Person person;
+		public Shell shell;
+		public startPerson(Person _person, Shell _shell){
+			person = _person;
+			shell = _shell;
+		}
+		public void run(){
+			PersonWindow per = new PersonWindow(shell, 0, person);
+	    	per.open();
+		}
+	}
+	
+	/**
+	 * Start Home window.
+	 * 
+	 * @author ericpinet
+	 * <br> 2015-09-13
+	 */
+	static class startHome implements Runnable{
+		public Home home;
+		public Shell shell;
+		public startHome(Home _home, Shell _shell){
+			home = _home;
+			shell = _shell;
+		}
+		public void run(){
+			HomeWindow per = new HomeWindow(shell, 0, home);
+	    	per.open();
+		}
+	}
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			MainWindow window = new MainWindow();
+			window.open();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(328, 210);
+		shell.setText("ConnectLife - Simulator");
+		shell.setLayout(new GridLayout(2, false));
+		new Label(shell, SWT.NONE);
+		new Label(shell, SWT.NONE);
+		
+		Label lblHost = new Label(shell, SWT.NONE);
+		lblHost.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblHost.setText("Host:");
+		
+		textHost = new Text(shell, SWT.BORDER);
+		textHost.setText(HOST);
+		textHost.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label lblPort = new Label(shell, SWT.NONE);
+		lblPort.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblPort.setText("Port:");
+		
+		textPort = new Text(shell, SWT.BORDER);
+		textPort.setText(PORT);
+		textPort.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		new Label(shell, SWT.NONE);
+		
+		Button btnConnect = new Button(shell, SWT.NONE);
+		btnConnect.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent arg0) {
+				
+				lblStatus.setText("Connecting to ConnectLife ...");
+				
+				connectionWithServer();
+				
+			}
+		});
+		btnConnect.setText("Connect");
+		new Label(shell, SWT.NONE);
+		
+		Button btnDisconnect = new Button(shell, SWT.NONE);
+		btnDisconnect.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent arg0) {
+				
+				lblStatus.setText("Disconnect ...");
+				
+				disconnect();
+				
+			}
+		});
+		btnDisconnect.setText("Disconnect");
+		//new Label(shell, SWT.NONE);
+		//new Label(shell, SWT.NONE);
+		new Label(shell, SWT.NONE);
+		
+		lblStatus = new Label(shell, SWT.NONE);
+		lblStatus.setText("Not connected");
+		lblStatus.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, true, 1, 1));
+
+	}
+	
+	/**
+	 * 
+	 */
+	private void connectionWithServer(){
+		
+		try {
+			client = new Client(textHost.getText(), Integer.parseInt(textPort.getText()), this);
+			String responseBody = client.getEnvironmentDataJson();
+		    
+		    // Deal with the response.
+			// Use caution: ensure correct character encoding and is not binary data
+			Gson gson = new Gson();
+			Environment env = gson.fromJson(new String(responseBody), Environment.class);
+			  
+			// open persons
+			Iterator<Person> itrp = env.getPersons().iterator();
+			while(itrp.hasNext()){
+				shell.getDisplay().asyncExec( new startPerson (itrp.next(), shell) );
+			}
+			  
+			// open homes
+			Iterator<Home> itrh = env.getHomes().iterator();
+			while(itrh.hasNext()){
+				shell.getDisplay().asyncExec( new startHome(itrh.next(), shell) );
+			}
+			
+			//client.close();
+			
+			/*
+			TTransport transport;
+			transport = new TSocket(textHost.getText(), Integer.parseInt(textPort.getText()));
+	        transport.open();
+	        
+	        TProtocol protocol = new  TBinaryProtocol(transport);
+	        CLApi.Client client = new CLApi.Client(protocol);
+	        
+	        
+	        CLApiPush.Iface handler = new CLApiPush.Iface() {
+				
+				public void push(Notification _notification) throws Xception, TException {
+					System.out.println(_notification.getData());
+				}
+			};
+	        
+			Processor<CLApiPush.Iface> processor = new CLApiPush.Processor<CLApiPush.Iface>(handler);
+			
+	        perform(client, shell);
+	        
+	        while(true)
+	        {
+		        try {
+					while (processor.process(protocol, protocol) == true) { System.out.println("Process");  }
+				} catch (TException e) {
+					System.out.println("Error");
+				}
+	        }
+
+	        //transport.close();
+	         
+	         */
+		}
+		catch( Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	private void disconnect(){
+		if(client != null)
+			client.close();
+		
+		lblStatus.setText("Not connected.");
+	}
+	
+	private static void perform(CLApi.Client client, Shell _shell) throws TException
+	{
+	    String responseBody = client.getEnvironmentDataJson();
+	    
+	    // Deal with the response.
+		// Use caution: ensure correct character encoding and is not binary data
+		Gson gson = new Gson();
+		Environment env = gson.fromJson(new String(responseBody), Environment.class);
+		  
+		// open persons
+		Iterator<Person> itrp = env.getPersons().iterator();
+		while(itrp.hasNext()){
+			_shell.getDisplay().asyncExec( new startPerson (itrp.next(), _shell) );
+		}
+		  
+		// open homes
+		Iterator<Home> itrh = env.getHomes().iterator();
+		while(itrh.hasNext()){
+			_shell.getDisplay().asyncExec( new startHome(itrh.next(), _shell) );
+		}
+	}
+
+	/**
+	 * @param _notification
+	 * @see com.connectlife.clapi.client.NotificationListener#notificationReceive(com.connectlife.clapi.Notification)
+	 */
+	public void notificationReceive(Notification _notification) {
+		System.out.println(_notification.getData());	
+	}
+
+}
---
+++ simulator/src/main/java/com/connectlife/simulator/PersonWindow.java
@@ -0,0 +1,243 @@
+/**
+ *  PersonWindow.java
+ *  simulator
+ *
+ *  Created by ericpinet on 2015-09-13.
+ *  Copyright (c) 2015 ConnectLife (Eric Pinet). All rights reserved.
+ *
+ */
+package com.connectlife.simulator;
+
+// external
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Label;
+
+import java.util.Iterator;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Text;
+
+import com.connectlife.coreserver.modules.environment.data.Address;
+import com.connectlife.coreserver.modules.environment.data.Email;
+// internal
+import com.connectlife.coreserver.modules.environment.data.Person;
+import com.connectlife.coreserver.modules.environment.data.PhoneNumber;
+
+
+/**
+ * 
+ * 
+ * @author ericpinet
+ * <br> 2015-09-13
+ */
+public class PersonWindow extends Dialog {
+
+	protected Object result;
+	protected Shell shell;
+	private Text txtUid;
+	private Text txtFirstname;
+	private Text txtLastname;
+	private Table tableEmail;
+	private Table tablePhoneNumbers;
+	private Table tableAddress;
+	
+	private Person person;
+
+	/**
+	 * Create the dialog.
+	 * @param parent
+	 * @param style
+	 */
+	public PersonWindow(Shell parent, int style, Person _person) {
+		super(parent, style);
+		setText("Person");
+		person = _person;
+	}
+
+	/**
+	 * Open the dialog.
+	 * @return the result
+	 */
+	public Object open() {
+		createContents();
+		shell.open();
+		shell.layout();
+		Display display = Display.getDefault();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Create contents of the dialog.
+	 */
+	private void createContents() {
+		shell = new Shell();
+		shell.setSize(600, 399);
+		shell.setText(getText());
+		shell.setLayout(new GridLayout(2, false));
+		
+		Label lblUid = new Label(shell, SWT.NONE);
+		lblUid.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblUid.setText("UID:");
+		
+		txtUid = new Text(shell, SWT.BORDER);
+		txtUid.setText(person.getUid());
+		txtUid.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label lblFirstName = new Label(shell, SWT.NONE);
+		lblFirstName.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblFirstName.setText("First name:");
+		
+		txtFirstname = new Text(shell, SWT.BORDER);
+		txtFirstname.setText(person.getFirstName());
+		txtFirstname.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label lblLastName = new Label(shell, SWT.NONE);
+		lblLastName.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblLastName.setText("Last name:");
+		
+		txtLastname = new Text(shell, SWT.BORDER);
+		txtLastname.setText(person.getLastName());
+		txtLastname.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label lblEmails = new Label(shell, SWT.NONE);
+		lblEmails.setText("Emails:");
+		
+		tableEmail = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
+		GridData gd_tableEmail = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
+		gd_tableEmail.heightHint = 89;
+		tableEmail.setLayoutData(gd_tableEmail);
+		tableEmail.setHeaderVisible(true);
+		tableEmail.setLinesVisible(true);
+		
+		TableColumn tblclmnEmail = new TableColumn(tableEmail, SWT.NONE);
+		tblclmnEmail.setWidth(300);
+		tblclmnEmail.setText("Email");
+		
+		TableColumn tblclmnType = new TableColumn(tableEmail, SWT.NONE);
+		tblclmnType.setWidth(100);
+		tblclmnType.setText("Type");
+		
+		// load table informations.
+		Iterator<Email> itr = person.getEmails().iterator();
+		while(itr.hasNext()){
+			Email email = itr.next();
+			String type = "";
+			if(Email.Type.PERSONAL == email.getType())
+				type = "Personal";
+			else if (Email.Type.WORK == email.getType())
+				type = "Work";
+			else if (Email.Type.OTHER == email.getType())
+				type = "Other";
+			
+			TableItem item = new TableItem(tableEmail, SWT.NONE);
+			item.setText(new String[]{email.getEmail(), type});
+		}
+		
+		Label lblPhoneNumbers = new Label(shell, SWT.NONE);
+		lblPhoneNumbers.setText("Phone numbers:");
+		
+		tablePhoneNumbers = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
+		GridData gd_tablePhoneNumbers = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
+		gd_tablePhoneNumbers.heightHint = 71;
+		tablePhoneNumbers.setLayoutData(gd_tablePhoneNumbers);
+		tablePhoneNumbers.setHeaderVisible(true);
+		tablePhoneNumbers.setLinesVisible(true);
+		
+		TableColumn tblclmnPhoneNumber = new TableColumn(tablePhoneNumbers, SWT.NONE);
+		tblclmnPhoneNumber.setWidth(300);
+		tblclmnPhoneNumber.setText("Phone number");
+		
+		TableColumn tblclmnType_1 = new TableColumn(tablePhoneNumbers, SWT.NONE);
+		tblclmnType_1.setWidth(100);
+		tblclmnType_1.setText("Type");
+		
+		// load table informations.
+		Iterator<PhoneNumber> itrp = person.getPhoneNumbers().iterator();
+		while(itrp.hasNext()){
+			PhoneNumber phone = itrp.next();
+			String type = "";
+			if(PhoneNumber.Type.CELL == phone.getType())
+				type = "Cellular";
+			else if(PhoneNumber.Type.HOME == phone.getType())
+				type = "Home";
+			else if (PhoneNumber.Type.WORK == phone.getType())
+				type = "Work";
+			else if (PhoneNumber.Type.OTHER == phone.getType())
+				type = "Other";
+			
+			TableItem item = new TableItem(tablePhoneNumbers, SWT.NONE);
+			item.setText(new String[]{phone.getPhoneNumber(), type});
+		}
+		
+		Label lblAddress = new Label(shell, SWT.NONE);
+		lblAddress.setText("Address:");
+		
+		tableAddress = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
+		GridData gd_tableAddress = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
+		gd_tableAddress.heightHint = 73;
+		tableAddress.setLayoutData(gd_tableAddress);
+		tableAddress.setHeaderVisible(true);
+		tableAddress.setLinesVisible(true);
+		
+		TableColumn tblclmnStreet = new TableColumn(tableAddress, SWT.NONE);
+		tblclmnStreet.setWidth(100);
+		tblclmnStreet.setText("Street");
+		
+		TableColumn tblclmnCity = new TableColumn(tableAddress, SWT.NONE);
+		tblclmnCity.setWidth(100);
+		tblclmnCity.setText("City");
+		
+		TableColumn tblclmnRegion = new TableColumn(tableAddress, SWT.NONE);
+		tblclmnRegion.setWidth(100);
+		tblclmnRegion.setText("Region");
+		
+		TableColumn tblclmnZipcode = new TableColumn(tableAddress, SWT.NONE);
+		tblclmnZipcode.setWidth(100);
+		tblclmnZipcode.setText("ZipCode");
+		
+		TableColumn tblclmnCountry = new TableColumn(tableAddress, SWT.NONE);
+		tblclmnCountry.setWidth(100);
+		tblclmnCountry.setText("Country");
+		
+		TableColumn tblclmnType_2 = new TableColumn(tableAddress, SWT.NONE);
+		tblclmnType_2.setWidth(100);
+		tblclmnType_2.setText("Type");
+		
+		// load table informations.
+		Iterator<Address> itra = person.getAddress().iterator();
+		while(itra.hasNext()){
+			Address address = itra.next();
+			String type = "";
+			if(Address.Type.HOME == address.getType())
+				type = "Home";
+			else if(Address.Type.WORK == address.getType())
+				type = "Work";
+			else if (Address.Type.OTHER == address.getType())
+				type = "Other";
+			
+			TableItem item = new TableItem(tableAddress, SWT.NONE);
+			item.setText(new String[]{address.getStreet(), address.getCity(), address.getRegion(), address.getZipCode(), address.getCountry() , type});
+		}
+		
+		Label lblPicture = new Label(shell, SWT.NONE);
+		lblPicture.setText("Picture:");
+		
+		// TODO Load image
+		@SuppressWarnings("unused")
+		Canvas canvas = new Canvas(shell, SWT.NONE);
+	}
+
+}
