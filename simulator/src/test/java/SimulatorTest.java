ccb838fb5b802a28590567a6a93190006712fd6c
Eric Pinet <pineri01@gmail.com>
2016-02-14 13:08:26 -0500

    Create the log4j2 for simulator.

---
+++ simulator/src/test/java/SimulatorTest.java
@@ -0,0 +1,59 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *  SimulatorTest.java
+ *  simulator
+ *
+ *  Created by ericpinet on 2016-02-14.
+ *  Copyright (c) 2016 ConnectLife (Eric Pinet). All rights reserved.
+ *
+ */
+
+/**
+ * 
+ * 
+ * @author ericpinet
+ * <br> 2016-02-14
+ */
+public class SimulatorTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
