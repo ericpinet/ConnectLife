9779926419adcb0c1c13ff2f8e1d3ff7bb911f07
Eric Pinet <pineri01@gmail.com>
2016-02-14 23:07:10 -0500

    Improve the simulator home window.
    Adding synchronization with the device in environment.

--- simulator/src/main/java/com/connectlife/simulator/HomeWindow.java
+++ simulator/src/main/java/com/connectlife/simulator/HomeWindow.java
@@ -26,6 +26,12 @@ import org.eclipse.swt.widgets.TreeColumn;
 import org.eclipse.swt.widgets.TreeItem;
 
 import com.clapi.data.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 
 /**
@@ -41,6 +47,8 @@ public class HomeWindow extends Dialog {
 	private Text txtUid;
 	private Text txtName;
 	private Home home;
+	private StyledText styledTextDetails;
+	private Tree tree;
 
 	/**
 	 * Create the dialog.
@@ -76,7 +84,7 @@ public class HomeWindow extends Dialog {
 	@SuppressWarnings("unused")
 	private void createContents() {
 		shell = new Shell();
-		shell.setSize(650, 450);
+		shell.setSize(650, 768);
 		shell.setText(getText());
 		shell.setLayout(new GridLayout(2, false));
 		
@@ -105,7 +113,64 @@ public class HomeWindow extends Dialog {
 		Label lblZones = new Label(shell, SWT.NONE);
 		lblZones.setText("Zones:");
 		
-		Tree tree = new Tree(shell, SWT.BORDER | SWT.FULL_SELECTION);
+		tree = new Tree(shell, SWT.BORDER | SWT.FULL_SELECTION);
+		tree.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				TreeItem[] items = tree.getSelection();
+				//only select the first
+				if( items[0] != null ){
+					Home home = null;
+					Zone zone = null;
+					Room room = null;
+					Accessory acc = null;
+					
+					// check if item is a home
+					try{
+						home = (Home)items[0].getData();
+						Gson gson = new GsonBuilder().setPrettyPrinting().create();
+						String json = gson.toJson(home);
+						styledTextDetails.setText(json);
+					}
+					catch(Exception e1){
+						// invalid type, do nothing.
+					}
+					
+					// check if item is a zone
+					try{
+						zone = (Zone)items[0].getData();
+						Gson gson = new GsonBuilder().setPrettyPrinting().create();
+						String json = gson.toJson(zone);
+						styledTextDetails.setText(json);
+					}
+					catch(Exception e1){
+						// invalid type, do nothing.
+					}
+					
+					// check if item is a room
+					try{
+						room = (Room)items[0].getData();
+						Gson gson = new GsonBuilder().setPrettyPrinting().create();
+						String json = gson.toJson(room);
+						styledTextDetails.setText(json);
+					}
+					catch(Exception e2){
+						// invalid type, do nothing.
+					}
+					
+					// check if item is a accessory
+					try{
+						acc = (Accessory)items[0].getData();
+						Gson gson = new GsonBuilder().setPrettyPrinting().create();
+						String json = gson.toJson(acc);
+						styledTextDetails.setText(json);
+					}
+					catch(Exception e3){
+						// invalid type, do nothing.
+					}
+				}
+			}
+		});
 		tree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		
 		TreeColumn trclmnName = new TreeColumn(tree, SWT.NONE);
@@ -117,33 +182,83 @@ public class HomeWindow extends Dialog {
 		trclmnUid.setText("Uid");
 		
 		TreeItem root = new TreeItem(tree, SWT.NONE);
+		root.setData(home);
 		root.setText(new String[] { "Home: " + home.getLabel(), home.getUid() });
 		
 		tree.setHeaderVisible(true);
-		
 		
 		Iterator<Zone> itr = home.getZones().iterator();
 		while(itr.hasNext()){
 			Zone zone = itr.next();
 			TreeItem item = new TreeItem(root, SWT.NONE);
+			item.setData(zone);
 		    item.setText(new String[] { "Zone: " + zone.getLabel(), zone.getUid() });
 		    
 		    Iterator<Room> itrr = zone.getRooms().iterator();
 		    while(itrr.hasNext()){
 		    	Room room = itrr.next();
 				TreeItem item2 = new TreeItem(item, SWT.NONE);
+				item2.setData(room);
 			    item2.setText(new String[] { "Room: " + room.getLabel(), room.getUid() });
 			    
 			    Iterator<Accessory> itra = room.getAccessories().iterator();
 			    while(itra.hasNext()){
 			    	Accessory acc = itra.next();
 					TreeItem item3 = new TreeItem(item2, SWT.NONE);
+					item3.setData(acc);
 				    item3.setText(new String[] { "Accessory: " + acc.getLabel(), acc.getUid() });
 			    }
 		    }
 		}
 		
 		tree.setTopItem(root);
+		
+		Label lblDetails = new Label(shell, SWT.NONE);
+		lblDetails.setText("Details:");
+		
+		styledTextDetails = new StyledText(shell, SWT.BORDER | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.READ_ONLY);
+		styledTextDetails.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+	}
+	
+	public void UpdateData(Home _home){
+		this.home = _home;
+		
+		txtUid.setText(home.getUid());
+		txtName.setText(home.getLabel());
+		
+		
+		
+		TreeItem root = tree.getTopItem();
+		root.setData(home);
+		root.setText(new String[] { "Home: " + home.getLabel(), home.getUid() });
+		root.removeAll();
+		
+		Iterator<Zone> itr = home.getZones().iterator();
+		while(itr.hasNext()){
+			Zone zone = itr.next();
+			TreeItem item = new TreeItem(root, SWT.NONE);
+			item.setData(zone);
+		    item.setText(new String[] { "Zone: " + zone.getLabel(), zone.getUid() });
+		    
+		    Iterator<Room> itrr = zone.getRooms().iterator();
+		    while(itrr.hasNext()){
+		    	Room room = itrr.next();
+				TreeItem item2 = new TreeItem(item, SWT.NONE);
+				item2.setData(room);
+			    item2.setText(new String[] { "Room: " + room.getLabel(), room.getUid() });
+			    
+			    Iterator<Accessory> itra = room.getAccessories().iterator();
+			    while(itra.hasNext()){
+			    	Accessory acc = itra.next();
+					TreeItem item3 = new TreeItem(item2, SWT.NONE);
+					item3.setData(acc);
+				    item3.setText(new String[] { "Accessory: " + acc.getLabel(), acc.getUid() });
+			    }
+		    }
+		}
+		
+		tree.setTopItem(root);
+		styledTextDetails.setText("");
 	}
 
 }
