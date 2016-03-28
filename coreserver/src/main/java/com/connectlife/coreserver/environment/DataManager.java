f65c6a8ed41753ca47397cfed8e8e07d802a187b
Eric Pinet <pineri01@gmail.com>
2016-03-28 17:42:59 -0400

    Fix javadoc

--- coreserver/src/main/java/com/connectlife/coreserver/environment/DataManager.java
+++ coreserver/src/main/java/com/connectlife/coreserver/environment/DataManager.java
@@ -103,7 +103,6 @@ public interface DataManager {
 	 * Set the new Data environment.
 	 * 
 	 * @param _data Set the new data environment.
-	 * @return Data of the environment.
 	 */
 	public void setData(Data _data);
 	
