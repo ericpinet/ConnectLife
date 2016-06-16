afa005df0451dd4a502230c0ca411cc475f9ca0f
ericpinet <pineri01@gmail.com>
2016-06-15 20:09:36 -0400

    Adding new tests.

--- coreserver/src/test/java/com/connectlife/test/coreserver/environment/cmd/CmdAddAccessoryTest.java
+++ coreserver/src/test/java/com/connectlife/test/coreserver/environment/cmd/CmdAddAccessoryTest.java
@@ -15,12 +15,27 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import com.clapi.data.Accessory;
+import com.clapi.data.Room;
+import com.connectlife.coreserver.Consts;
 import com.connectlife.coreserver.environment.EnvironmentContext;
 import com.connectlife.coreserver.environment.cmd.CmdAddAccessory;
 import com.connectlife.coreserver.environment.cmd.CmdFactory;
+import com.connectlife.coreserver.environment.data.DataManager;
+import com.connectlife.coreserver.environment.data.DataManagerNodeFactory;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(DataManagerNodeFactory.class)
+@PowerMockIgnore("javax.management.*")
 
 /**
  * 
@@ -102,9 +117,18 @@ public class CmdAddAccessoryTest {
 	 * Test without uid set in accessory but room null
 	 */
 	@Test
-	public void testWithoutUIDAccessory() {
+	public void testWithoutUIDRoomNullAccessory() {
 		
 		EnvironmentContext context = Mockito.mock(EnvironmentContext.class);
+		DataManager datamanager = Mockito.mock(DataManager.class);
+		GraphDatabaseService service = Mockito.mock(GraphDatabaseService.class);
+
+		Mockito.when(context.getDataManager()).thenReturn(datamanager);
+		try {
+			Mockito.when(datamanager.getGraph()).thenReturn(service);
+		} catch (Exception e1) {
+			fail();
+		}
 		
 		Accessory accessory = new Accessory("", "Label");
 		
@@ -116,6 +140,113 @@ public class CmdAddAccessoryTest {
 			fail();
 		} catch (Exception e) {
 			assertNotNull(e);
+		}
+	}
+	
+	/**
+	 * Test without uid set in accessory and valid room (don't find room)
+	 */
+	@Test
+	public void testWithoutUIDAccessoryDontFindRoom() {
+		
+		EnvironmentContext context = Mockito.mock(EnvironmentContext.class);
+		DataManager datamanager = Mockito.mock(DataManager.class);
+		GraphDatabaseService service = Mockito.mock(GraphDatabaseService.class);
+
+		Mockito.when(context.getDataManager()).thenReturn(datamanager);
+		try {
+			Mockito.when(datamanager.getGraph()).thenReturn(service);
+		} catch (Exception e1) {
+			fail();
+		}
+		
+		Accessory accessory = new Accessory("", "Label");
+		Room room = new Room("12345", "Label");
+		
+		CmdAddAccessory cmd = CmdFactory.getCmdAddAccesssory(accessory, room);
+		cmd.setContext(context);
+
+		try {
+			cmd.execute();
+			fail();
+			
+		} catch (Exception e) {
+			assertNotNull(e);
+		}
+	}
+	
+	/**
+	 * Test without uid set in accessory already added
+	 */
+	@Test
+	public void testWithoutUIDAccessoryAccessoryAlreadyAdded() {
+		
+		EnvironmentContext context = Mockito.mock(EnvironmentContext.class);
+		DataManager datamanager = Mockito.mock(DataManager.class);
+		GraphDatabaseService graph = Mockito.mock(GraphDatabaseService.class);
+		Node acc = Mockito.mock(Node.class);
+
+		Mockito.when(context.getDataManager()).thenReturn(datamanager);
+		Mockito.when(graph.findNode(Consts.LABEL_ACCESSORY, Consts.ACCESSORY_SERIALNUMBER, "")).thenReturn(acc);
+		try {
+			Mockito.when(datamanager.getGraph()).thenReturn(graph);
+		} catch (Exception e1) {
+			fail();
+		}
+		
+		Accessory accessory = new Accessory("", "Label");
+		Room room = new Room("12345", "Label");
+		
+		CmdAddAccessory cmd = CmdFactory.getCmdAddAccesssory(accessory, room);
+		cmd.setContext(context);
+
+		try {
+			cmd.execute();
+			fail();
+			
+		} catch (Exception e) {
+			assertNotNull(e);
+		}
+	}
+	
+	/**
+	 * Test without uid set in accessory already added
+	 */
+	@Test
+	public void testComplete() {
+		
+		EnvironmentContext context = Mockito.mock(EnvironmentContext.class);
+		DataManager datamanager = Mockito.mock(DataManager.class);
+		GraphDatabaseService graph = Mockito.mock(GraphDatabaseService.class);
+		Node node = Mockito.mock(Node.class);
+		Node acc = Mockito.mock(Node.class);
+		
+		PowerMockito.mockStatic(DataManagerNodeFactory.class);
+		try {
+			Mockito.when(DataManagerNodeFactory.buildAccessoryNode(Mockito.any(), Mockito.any())).thenReturn(acc);
+		} catch (Exception e2) {
+			fail();
+		}
+
+		Mockito.when(context.getDataManager()).thenReturn(datamanager);
+		Mockito.when(graph.findNode(Consts.LABEL_ROOM, Consts.UID, "12345")).thenReturn(node);
+		try {
+			Mockito.when(datamanager.getGraph()).thenReturn(graph);
+		} catch (Exception e1) {
+			fail();
+		}
+		
+		Accessory accessory = new Accessory("", "Label");
+		Room room = new Room("12345", "Label");
+		
+		CmdAddAccessory cmd = CmdFactory.getCmdAddAccesssory(accessory, room);
+		cmd.setContext(context);
+
+		try {
+			cmd.execute();
+			
+		} catch (Exception e) {
+			fail();
 		}
 	}
 }
