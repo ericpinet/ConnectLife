62d1036cc87434f230c312062d5a079d1419081b
Eric Pinet <pineri01@gmail.com>
2016-09-10 21:10:56 -0400

    AssetHttpHandlerTest improvement

--- coreserver/src/test/java/com/connectlife/test/coreserver/environment/asset/AssetHttpHandlerTest.java
+++ coreserver/src/test/java/com/connectlife/test/coreserver/environment/asset/AssetHttpHandlerTest.java
@@ -8,6 +8,8 @@
  */
 package com.connectlife.test.coreserver.environment.asset;
 
+import static org.junit.Assert.*;
+
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -20,12 +22,24 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import com.clapi.data.Asset;
+import com.connectlife.coreserver.Application;
+import com.connectlife.coreserver.environment.Environment;
 import com.connectlife.coreserver.environment.asset.AssetHttpHandler;
 import com.connectlife.coreserver.environment.asset.AssetMngr;
+import com.connectlife.coreserver.environment.find.FindProcessor;
 
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Application.class)
+@PowerMockIgnore("javax.management.*")
 /**
  * 
  * 
@@ -63,24 +77,40 @@ public class AssetHttpHandlerTest {
 	}
 
 	@Test
-	public void testUpdate() {
+	public void testHandle() {
+		
+		PowerMockito.spy(Application.class);
 		
 		AssetMngr manager = Mockito.mock(AssetMngr.class);
 		
 		Request base_request = Mockito.mock(Request.class);
 		HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
 		HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+		FindProcessor find = Mockito.mock(FindProcessor.class);
+		Asset asset = Mockito.mock(Asset.class);
+		Application app = Mockito.mock(Application.class);
+		Environment env = Mockito.mock(Environment.class);
+		try {
+			PowerMockito.doReturn(app).when(Application.class, "getApp");
+			Mockito.when(app.getEnvironment()).thenReturn(env);
+			Mockito.when(env.getFindProcessor()).thenReturn(find);
+			Mockito.when(request.getParameter("uid")).thenReturn("1");
+			Mockito.when(find.findAssetByUid("1")).thenReturn(asset);
+			Mockito.when(manager.getAssetFullFilename(asset)).thenReturn("filename");
+			
+		} catch (Exception e1) {
+			fail();
+		}
 		
 		AssetHttpHandler http = new AssetHttpHandler(manager);
-		
 		try {
 			http.handle("", base_request, request, response);
 			
-			
 		} catch (IOException e) {
+			fail();
 			
 		} catch (ServletException e) {
-
+			fail();
 		}
 	}
 }
